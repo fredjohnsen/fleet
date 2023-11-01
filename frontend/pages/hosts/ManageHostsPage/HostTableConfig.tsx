4d56aa8c139508e3dd0de47968e36c4d1a1f1e29
Jahziel Villasana-Espinoza <jahziel@fleetdm.com>
2023-11-01 17:25:34 -0400

    feat: use negative value to match product's concept of sort order

--- frontend/pages/hosts/ManageHostsPage/HostTableConfig.tsx
+++ frontend/pages/hosts/ManageHostsPage/HostTableConfig.tsx
@@ -88,13 +88,13 @@ const condenseDeviceUsers = (users: IDeviceUser[]): string[] => {
   const condensed =
     users.length === 4
       ? users
-          .slice(-4)
-          .map((u) => u.email)
-          .reverse()
+        .slice(-4)
+        .map((u) => u.email)
+        .reverse()
       : users
-          .slice(-3)
-          .map((u) => u.email)
-          .reverse() || [];
+        .slice(-3)
+        .map((u) => u.email)
+        .reverse() || [];
   return users.length > 4
     ? condensed.concat(`+${users.length - 3} more`) // TODO: confirm limit
     : condensed;
@@ -351,9 +351,8 @@ const allHostTableHeaders: IDataColumn[] = [
         return (
           <>
             <span
-              className={`text-cell ${
-                users.length > 1 ? "text-muted tooltip" : ""
-              }`}
+              className={`text-cell ${users.length > 1 ? "text-muted tooltip" : ""
+                }`}
               data-tip
               data-for={`device_mapping__${cellProps.row.original.id}`}
               data-tip-disable={users.length <= 1}
@@ -580,7 +579,7 @@ const allHostTableHeaders: IDataColumn[] = [
         isLastColumn={cellProps.column.isLastColumn}
       />
     ),
-    accessor: "uptime",
+    accessor: "last_restarted",
     Cell: (cellProps: ICellProps) => {
       const { uptime, detail_updated_at, platform } = cellProps.row.original;
 
